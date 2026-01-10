a3be92ceffec3ddc83ec616711851708fd3b2c22
TheAssassin001 <aswinkjayan2002@gmail.com>
2026-01-10 09:26:42 +0530

    Navbar: make logo even larger for visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar: React.FC<NavbarProps> = ({ onOpenWaitlist }) => {
             <img
               src="/Talent AI Logo.png"
               alt="Talent AI Logo"
-              className="w-20 h-20 md:w-24 md:h-24 object-contain"
+              className="w-28 h-28 md:w-32 md:h-32 object-contain"
               style={{ display: 'block', background: 'none', boxShadow: 'none', border: 'none', borderRadius: '0', padding: '0', margin: '0' }}
             />
           </div>
